5d413b9a9a1460ed962b2269db03f092b4601ce1
Krishna Prabhu <krishnaprabhu21@gmail.com>
2015-12-11 15:55:45 +0530

    layout completed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,62 @@
-var dropdown_tooltip = $('#menu_tooltip');
-var dropdown = $('.dropdown');
 
-var search = $('#search');
-var search_from = $('.navbar-form');
+$(document).ready(function() {
 
-$(dropdown).click(function(){
-    $(dropdown_tooltip).toggle();
-});
+	var dropdown_tooltip = $('#menu_tooltip');
+	var dropdown = $('.dropdown');
+
+	var search = $('#search');
+	var search_from = $('.navbar-form');
+
+	$(dropdown).click(function(){
+	    $(dropdown_tooltip).toggle();
+	});
+
+	$(search).click(function(){
+	    $(search_from).toggle(500);
+	});	
 
 
-$(search).click(function(){
-    $(search_from).toggle(500);
+	// Custom Slider
+	$('.slide_items').first().addClass('active');
+	$('.slide_items').hide();
+	$('.active').show();
+
+	//prev btn click
+	$('#btnprev').click(function(event) {		
+		$('.active').removeClass('active').addClass('old_active');
+
+		if($('.old_active').is(':first-child')) {
+			$('.slide_items').last().addClass('active')
+		}
+		else {
+			$('.old_active').prev().addClass('active');
+		}
+
+		$('.old_active').removeClass('old_active');
+		$('.slide_items').fadeOut();
+		$('.active').fadeIn();
+	});	
+
+
+	//next btn click
+	$('#btnnext').click(function(event) {		
+		$('.active').removeClass('active').addClass('old_active');
+
+		if($('.old_active').is(':last-child')) {
+			$('.slide_items').first().addClass('active')
+		}
+		else if($('.active').is(':first-child')) {
+			alert('A');
+		}
+		else {
+			$('.old_active').next().addClass('active');
+		}
+
+		$('.old_active').removeClass('old_active');
+		$('.slide_items').fadeOut();
+		$('.active').fadeIn();
+	});	
+
+
 });
 
